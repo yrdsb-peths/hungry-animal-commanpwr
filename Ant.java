3eba0951734205619aaf34972eb1f7a88e959af1
commanpwr <101415341+commanpwr@users.noreply.github.com>
2022-05-04 10:28:20 -0400

    error with Ant

--- Ant.java
+++ Ant.java
@@ -1,19 +1,29 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Ant here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Ant extends Actor
 {
-    /**
-     * Act - do whatever the Ant wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         // Add your action code here.
+        if(Greenfoot.isKeyDown("a"))
+        {
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d"))
+        {
+            move(2);
+        }
+        
+        eat();
+    }
+    
+    public void eat()
+    {
+        if(isTouching(Bread.class))
+        {
+            removeTouching(Bread.class);
+            MyWorld world = (MyWorld) getWorld();
+            world.spawnBread();
+        }
     }
 }
--- Bread.java
+++ Bread.java
@@ -1,19 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Bread here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Bread extends Actor
 {
-    /**
-     * Act - do whatever the Bread wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         // Add your action code here.
+        int x = getX();
+        int y = getY();
+        setLocation(x, y + 2);
     }
 }
